6a3b40b1b192ba84e100738c5f7aea28d2dd9cef
pujadahal <puja@apen4ej.org>
2019-06-20 20:32:23 -0700

    edits to smarttoken

--- public_html/index.php
+++ public_html/index.php
@@ -10,9 +10,10 @@ $smarty = new Smarty;
 // assign some content. This would typically come from
 // a database or other source, but we'll use static
 // values for the purpose of this example.
-$smarty->assign('name', 'firstly lastly');
+$smarty->assign('name', 'Puja Dahal');
 $smarty->assign('address', '1305 Franklin St');
-$smarty->assign('cell_phone', '');
+$smarty->assign('cell_phone', '510-236-4616');
+
 
 // display it
 $smarty->display('index.tpl');
